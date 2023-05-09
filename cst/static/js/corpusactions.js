9604ca637b934ceb49f8ca47264760c3644977a1
Sara Veldhoen <sara.veldhoen@kb.nl>
2023-05-09 09:02:26 +0200

    Delete corpus card - frontend properly

--- cst/static/js/corpusactions.js
+++ cst/static/js/corpusactions.js
@@ -5,19 +5,13 @@ $(document).ready(function() {
   $('.delete').click(function() {
     // TODO: dialog "Are you sure you want to delete corpus?" Alternative: install rubbish bin
     var corpusid = $(this).attr('data-id');
-    var corpuscard = $('.corpuscard[data-id='+corpusid+']'); // Am I selecting the right corpus card??
-    console.log('Delete corpus '+corpusid);
-    console.log('Corresponding corpus card '+corpuscard+ corpuscard.attr('data-id'));
-    corpuscard.css("color","yellow");
-    corpuscard.css("border", "5px solid red");
+    var corpuscard = $('#corpuscard-'+corpusid);
     $.post('delete-corpus',
     {
       corpusid: corpusid,
     },
     function(data, status){
-      alert("Data: " + data + "\nStatus: " + status);
-      corpuscard.remove() // Why is this not working??
-      location.reload();  // Ugly alternative is working
+      corpuscard.remove()
     });
   });
   });
