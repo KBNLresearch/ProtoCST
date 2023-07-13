a5ab3ed1215c8eb9185012d249d5c441e1742ac8
Sara Veldhoen <sara.veldhoen@kb.nl>
2023-07-13 15:28:50 +0200

    WIP, will not finish

--- dataprocessing/ingest.py
+++ dataprocessing/ingest.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import os
-import cst
 from cst import db
+import sqlite3
 
 def df_from_didl_soup(soup):
     resource_metadata = soup.find('srw_dc:dcx')
@@ -21,15 +21,28 @@ def df_from_didl_soup(soup):
 def ingest_from_directory(dir_path, replace = False):
     all_metadata = []
     for fn in os.listdir(dir_path):
-        if not os.path.isfile(fn) or not fn.endswith('.xml'):
+        if not os.path.isfile(os.path.join(dir_path, fn)) or not fn.endswith('.xml'):
+            print(f'Skip {fn}')
             continue
         with open(os.path.join(dir_path, fn), "r", encoding="utf8") as f:
             didl_soup = BeautifulSoup("".join(f.readlines()), "xml")
             all_metadata.append(df_from_didl_soup(didl_soup))
     df = pd.DataFrame(all_metadata)
-    with db.get_db() as con:
-        df.to_sql("document",con, if_exists='replace' if replace else 'append')
+    to_db(df,'document', replace = replace)
+    #with db.get_db() as con:
+    #    df.to_sql("document",con, if_exists='replace' if replace else 'append')
+
+
+
+
+def to_db(df, table_name, db = 'instance/cst.sqlite', replace = False):
+    with sqlite3.connect(db) as con:
+        df.to_sql(table_name, con, if_exists='replace' if replace else 'append')
+    print(f"Pushed {len(df)} entries to table {table_name}.")
+
+
+
 
 if __name__ == '__main__':
-    with cst.create_app() as app:
-        ingest_from_directory('harvested_data/metadata')
+
+    df = ingest_from_directory('dataprocessing/harvested_data/metadata', replace=True)
